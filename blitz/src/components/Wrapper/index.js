e7fccd54f8d1cf87f5465969618ee76feeec6237
VaseninaNastya <dadasiegt@yandex.ru>
2021-02-08 00:48:33 +0300

    feat: tablet

--- blitz/src/components/Wrapper/index.js
+++ blitz/src/components/Wrapper/index.js
@@ -2,7 +2,7 @@ import s from './Wrapper.module.css'
 import React from "react";
 import classNames from 'classnames';
 const Wrapper = ({colorTheme, children}) => {
-    const classes = colorTheme? classNames(s.wrapper, s.wrapper__white):classNames(s.wrapper, s.wrapper_gray);
+    const classes = colorTheme? classNames(s.wrapper, s.wrapper__white):classNames(s.wrapper, s.wrapper__gray);
     return (
         <div className = {classes}>
                 {children}
